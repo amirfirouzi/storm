75cf7ac32f7fed466b1f3333e05360cb3163da37
AMir Firouzi <firouziam@gmail.com>
2017-07-14 01:10:32 +0430

    working on storm-deploy.sh-scp and extracting done

--- scripts/storm-deploy.sh
+++ scripts/storm-deploy.sh
@@ -2,7 +2,11 @@
 
 user='storm'
 nodes='stormmaster stormslave1 stormslave2 stormslave3 stormslave4 stormslave5 stormslave6'
- 
+stormhome='/home/amir/Projects/storm/mystorm/storm'
+pkgname='apache-storm-2.0.0-SNAPSHOT'
+pkgnameext="$pkgname.tar.gz"
+stormpkg="storm-dist/binary/final-package/target/$pkgnameext"
+full=1
 check_failure() {
   #compile check
   if [ $? != 0 ]; then
@@ -28,10 +32,13 @@ sshcommand() {
 ssh "$1@$2" "$3"
 }
 
+sudosshcommand() {
+ssh -t "$1@$2" "sudo $3"
+}
 #mvn clean install -DskipTests=true
 #check_failure "compiling"
 
-#cd storm-dist/binary && mvn package
+#cd storm-dist/binary && mvn package && cd ../..
 #check_failure "packaging binaries"
 
 backupdir='backup'
@@ -53,19 +60,29 @@ do
         #make tar file form current storm directory and remove existing-name:storm-timestamp.tar.gz
         #make copy of slave storm.yaml file in backup/
         echo "creating backup on $i"
-        echo "removing last backup file"
+        echo "removing latest backup file"
         sshcommand $user $i "rm  $backupdir/storm-backup.tar.gz"
+        #TODO: if not exists storm.yaml in backup dir
         echo "copying storm.yaml file"
         sshcommand $user $i "cp $stormdir/conf/storm.yaml $backupdir/"
-        echo "making tar file"
-        sshcommand $user $i "tar -czf $backupdir/storm-backup.tar.gz storm-current/*"
-        
+#        echo "making tar file"
+#        sshcommand $user $i "tar -czf $backupdir/storm-backup.tar.gz $stormdir/*"
+
+        #remove storm-current directory content
+        echo "removing storm-current content"
+        sshcommand $user $i "rm -rf $stormdir/*"
+
         #copy new package to node at storm-current location
-        
-        #unzip package
-        
-        #copy yaml files to specified location: storm-current/conf
-        #stormmaster/storm.yaml for master nodes & stormslaves/storm.yaml for slave nodes
+        echo "copying new tar package"
+        cd $stormhome
+        if [ $full != 0 ]; then
+            scp $stormpkg "$user@$i:$stormdir/"
+            echo "extracting storm package"
+            ssh -t "$user@$i" "/usr/bin/sudo bash -c 'tar xzf $stormdir/$pkgnameext --strip 1 -C $stormdir/; rm $stormdir/$pkgnameext;chown -R $user:$user $stormdir/*; chmod -R 775 $stormdir/*;'"
+            #TODO: copy backup/storm.yaml to conf dir
+            #TODO: copy db.ini to conf dir
+#        else
+        fi
         
         #restart services: nimbus/supervisor and supervisor daemnons
     fi
